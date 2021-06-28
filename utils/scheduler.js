69a5dcb1d415b666defbc7a30bbc249ec0c604e1
Abdullah Jamal <aj941ga@gmail.com>
2021-06-28 19:03:21 +0530

    fixed bug in recurring schedule

--- utils/scheduler.js
+++ utils/scheduler.js
@@ -78,7 +78,7 @@ function validateSchedule(schedule) {
     if (scheduled === "recurring") {
 
         second = '*/20';
-        month = '*';
+        minute = '*';
         hour = '*';
         monthDay = '*';
         month = '*';
